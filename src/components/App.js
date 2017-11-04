c50b14a6c3cc6c12fe0061ce107674af2fea1b1b
rivermanvr <vrgithub@riversconsulting.com>
2017-11-04 11:19:27 -0400

    just doing setup at the moment

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import TestBed from './TestBed';
 import Navbar from '../containers/NavBar';
 import Tests from '../containers/Tests';
 import Test from '../containers/Test';
@@ -8,14 +9,14 @@ import Test from '../containers/Test';
 export default function AppContainer() {
   return (
     <div className="container-fluid">
-      <h3>Welcome to My Redux Template</h3>
+      <h3>Welcome to Dita</h3>
       <Route render={ (router) => <Navbar router={ router } /> } />
       <Switch>
       <Route
         path="/tests/:id" render={ (router) => <Test router={ router } /> }
       />
-      <Route
-        path="/" component={ Tests } />
+      <Route path="/testBed" component={ TestBed } />
+      <Route path="/" component={ Tests } />
     </Switch>
     </div>
   )
---
+++ src/components/TestBed.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+export default class TestBed extends Component {
+  constructor() {
+    super();
+    this.state = { id: 0, name: '', errorAdd: '' };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  componentDidMount(){
+    const id = this.props.router.match.params.id;
+    this.props.gotSingleTest(id)
+    .then(() => {
+      this.setState({
+        id: this.props.selectedTest.id,
+        name: this.props.selectedTest.name,
+        errorAdd: ''
+      })
+    })
+  }
+
+  componentWillReceiveProps(nextProps) {
+    //there is also: currentProps
+    if (!nextProps.selectedTest.id) return;
+    this.setState({
+      id: nextProps.selectedTest.id,
+      name: nextProps.selectedTest.name,
+      errorAdd: ''
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (this.state.name) {
+      const testChg = this.state;
+      testChg.photo = this.props.selectedTest.photo;
+      this.props.updateTest(testChg);
+    } else {
+      this.setState({ errorAdd: 'Name cannot be blank', name: this.state.test.name });
+    }
+  }
+
+  handleInput(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    switch (name) {
+      case 'name':
+        this.setState({ name: value });
+        break;
+      case 'phone':
+        this.setState({ phone: value });
+        break;
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        our testBed component
+      </div>
+    )
+  }
+}

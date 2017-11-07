cc6935d5f74678bfb847803b16f561edef4afb3f
rivermanvr <vrgithub@riversconsulting.com>
2017-11-07 13:14:46 -0500

    done

--- src/components/testerRoutines/TestBedGoogle.js
+++ src/components/testerRoutines/TestBedGoogle.js
@@ -5,7 +5,7 @@ import MapWithASearchBox from '../singleFunction/geoLocator';
 export default class TestBed extends Component {
   constructor() {
     super();
-    this.state = { places: [] };
+    this.state = { place: [] };
 
     this.handleInput = this.handleInput.bind(this);
   }
@@ -15,10 +15,16 @@ export default class TestBed extends Component {
   }
 
   render() {
-    
     return (
       <div>
         <h4>TestBedGoogle - geoLocationInput testing:</h4>
+        <ul>
+        <li>div wrap & render: geoLocator.js component</li>
+        <li>Look at the TestBedGoogle.js component to see what is passed into this component</li>
+        <li>Once a location is selected, the location is past to parent.</li>
+        <li>Once a place is acquired, a redirect should occur (...unmounting the geoLocator component).</li>
+        <li><hr /></li>
+      </ul>
         <div className="colWidth40">
           <MapWithASearchBox selection={ this.handleInput } />
         </div>

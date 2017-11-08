9b5509bb2b5821a9593105130f5ed7caf475992b
kaz-a <zakscloset@gmail.com>
2017-11-07 20:24:35 -0500

    showing ALL posts

--- src/components/singleFunction/AllPosts.js
+++ src/components/singleFunction/AllPosts.js
@@ -14,8 +14,17 @@ class AllPosts extends Component {
 
     return (
       <div>
-        <h4>All posts</h4>
-        { posts.posts.map(post => <PostCard key={ post.id } content={ post } />) }           
+      <h4>All posts</h4>
+      { 
+        posts.posts.map(post => {
+          return (
+            <div className="list-group-item">
+              <h4 className="list-group-item-heading">{ post.title } </h4>
+              <p className="list-group-item-text">{ post.body }</p>
+            </div>
+          )
+        })
+      }           
       </div>
     )
   }    

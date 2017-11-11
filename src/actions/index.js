d31025800b4c6b02987b2a8fa84cdab86ec6a987
kaz-a <zakscloset@gmail.com>
2017-11-11 17:54:31 -0500

    add-post should be working now \(^_^)/

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,3 @@
 export * from './authactions';
 export * from './userpostsactions'
-export * from './posts';
 export * from './storylines';
--- src/actions/userpostsactions.js
+++ src/actions/userpostsactions.js
@@ -2,15 +2,27 @@ import axios from 'axios';
 
 // ***** ACTION TYPES *****
 export const SET_USER_POSTS = 'SET_USER_POSTS'
+export const GET_POSTS = 'GET_POSTS';
 
 // ***** ACTION CREATORS *****
 export const setUserPosts = userPosts => ({ type: SET_USER_POSTS, userPosts })
+export const getPosts = (posts) => ({ type: GET_POSTS, posts })
 
 export const fetchUserPosts = () => dispatch =>
   axios.get('/api/posts/myposts')
     .then(res => dispatch(setUserPosts(res.data)))
 
+export const fetchPosts = () => dispatch => 
+  axios.get('/api/posts')
+    .then(res => dispatch(getPosts(res.data)))
+    .catch(console.log);
+
 export const addUserPost = (newPost) => dispatch => 
   axios.post('/api/posts/myposts', newPost)
     .then(res => dispatch(fetchUserPosts(res.data)))
+    .then(res => dispatch(fetchPosts(res.data)))
     .catch(console.log);
+
+
+
+

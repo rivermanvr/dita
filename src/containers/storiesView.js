5e08d4f06a4ca685c3b630ef90e7538c0d87e48c
rivermanvr <vrgithub@riversconsulting.com>
2017-11-16 15:44:01 -0500

    getting ready to display the storyline

--- src/containers/storiesView.js
+++ src/containers/storiesView.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 class storiesView extends Component {
   constructor() {
     super();
-    this.state = { userStorylines: [], userPosts: [], currentUser: {}, allPosts: [], toggle: 'stories' };
+    this.state = { userStorylines: [], userPosts: [], currentUser: {}, allPosts: [], toggle: 'stories', indexView: 0 };
   }
 
   componentDidMount () {
@@ -48,27 +48,43 @@ class storiesView extends Component {
     return (
       <div className="container marginT marginB">
         <div className="row marginB">
-          <div className="col-sm-2 center">
-            <button className="btn btn-default" disabled>Create a new Story</button>
+          <div className="col-xs-12">
+            <div className="col-xs-3 center">
+              <button className="btn btn-default" disabled>New Story</button>
+            </div>
+            <div className="col-xs-6 btn-group center" role="group" aria-label="poststory">
+              <Link to="/postsView"><button type="button" className="btn btn-default col-xs-6 center">Posts</button></Link>
+              <Link to="/storiesView"><button type="button" className="btn btn-primary col-xs-6 center">Stories</button></Link>
+            </div>
+            <div className="col-xs-3 center">
+              <button className="btn btn-default" disabled>Graphics Version</button>
+            </div>
           </div>
-          <div className="col-sm-8 btn-group center" role="group" aria-label="poststory">
-            <Link to="/postsView"><button type="button" className="btn btn-default col-sm-6 center">Posts</button></Link>
-            <Link to="/storiesView"><button type="button" className="btn btn-primary col-sm-6 center">Stories</button></Link>
-          </div>
-          <div className="col-sm-2 center">
-            <button className="btn btn-default" disabled>Graphics Version</button>
-          </div>
-        </div>
-        <div className="row col-sm-12 center marginB marginTSM">
-          <div className="col-sm-2 center"><h5>{ state.userStorylines.length } Storyline(s)</h5></div>
-          <div className="col-sm-1">traverse bar here</div>
         </div>
 
-<div className="row col-sm-12 panel panel-default">
-  <div>
-  hello
-  </div>
-</div>
+        <div className="row marginB marginTSM">
+          <div className="col-xs-12">
+            <div className="col-xs-3 center">
+              <button className="moveRight padLR20"><span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>  first </button>
+            </div>
+            <div className="col-xs-3 center">
+              <button className="moveRight"><span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span> previous</button>
+            </div>
+            <div className="col-xs-3 center">
+              <button className="moveRight padLR20"><span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>  next </button>
+            </div>
+              <div className="col-xs-3 center">
+              <button className="moveRight padLR20"><span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> last</button>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="row col-sm-12 panel panel-default">
+          <div>
+          hello
+          </div>
+        </div>
 
         <div className="row col-sm-12">
           <h4><p>View = { toggle }</p></h4>

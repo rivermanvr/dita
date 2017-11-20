84c03bb5357eba013048d362c5bf5e7b5afb8313
kaz-a <zakscloset@gmail.com>
2017-11-20 17:16:51 -0500

    points showing on map

--- src/components/singleFunction/AllPostsMap.js
+++ src/components/singleFunction/AllPostsMap.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import GeoLoc from './geoMapDisplay';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Map, Marker, Popup, TileLayer, CircleMarker } from 'react-leaflet';
+
 
 class AllPostsMap extends Component {
   constructor(props){
@@ -12,26 +12,60 @@ class AllPostsMap extends Component {
     const { posts, currentView } = this.props;    
     const zoomLevel = 2;
     const position = [currentView.lat, currentView.lng];
-    const darkTiles = 'https://api.mapbox.com/styles/v1/zakscloset/cja04we4b96b22sphpuhp5xil/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiemFrc2Nsb3NldCIsImEiOiI0Y2Q2ZDNmNjZhYzZkMzE5Y2FjNTEwY2YxZmVjMWZiYyJ9.TN1BPlB18BT4k5-GJnWrfw'
+    const darkTiles = 'https://api.mapbox.com/styles/v1/zakscloset/cja04we4b96b22sphpuhp5xil/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiemFrc2Nsb3NldCIsImEiOiI0Y2Q2ZDNmNjZhYzZkMzE5Y2FjNTEwY2YxZmVjMWZiYyJ9.TN1BPlB18BT4k5-GJnWrfw';
     const tileAttr = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
-    
+
+    const stars = posts.map(post => {
+      let latLng; 
+      if(post.latitude && post.longitude) latLng = [post.latitude, post.longitude]
+      else latLng = [0, 0]
+      return { post: post, latLng: latLng }
+    })
+
+    const fillColor = 'yellow', fillOpacity = 1, strokeColor = '#000', strokeWeight = 1;
+    let radius = 5 
+    const spanStyle = { fontSize: '1.5em' }
+
     return (
-      <div>
+      <div className="map">
         <Map center={ position } zoom={ zoomLevel } style={{ height: "600px", width: "100%" }}>
           <TileLayer
             attribution={ tileAttr }
             url={ darkTiles }
           />
+          
           <Marker position={ position }>
             <Popup>
-              <span>lat: { currentView.lat } <br/>lng: { currentView.lng }</span>
+              <div>
+                <span style={ spanStyle }>You are at</span><br/>
+                <span>lat: { currentView.lat } <br/>lng: { currentView.lng }</span>
+              </div>
             </Popup>
           </Marker>
+
+          {
+            stars && stars.map(star => {
+              return (                
+                <CircleMarker key={ star.post.id } center={ star.latLng } 
+                  radius={ radius } color = { strokeColor } fillColor={ fillColor } 
+                  fillOpacity={ fillOpacity } weight={ strokeWeight } weid>
+                  <Popup>
+                    <div>
+                      <span style={ spanStyle }>{ star.post.title }</span> <br/>
+                      <span>{ star.post.body }</span>
+                      </div>
+                  </Popup>
+                </CircleMarker>
+              )
+            })
+          }
+          
         </Map>
       </div>
     )
   }  
 }
+
 
 const mapStateToProps = ({ posts, currentView }) => {
   return {
@@ -40,3 +74,5 @@ const mapStateToProps = ({ posts, currentView }) => {
 }
 
 export default connect (mapStateToProps)(AllPostsMap)
+
+

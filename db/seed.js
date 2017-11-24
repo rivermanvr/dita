3f3315db8410ab3dd586670ac48100772796e647
Wasif Zaman <teatocode@gmail.com>
2017-11-24 15:23:40 -0500

    seed update with locations

--- db/seed.js
+++ db/seed.js
@@ -2,13 +2,20 @@ const db = require('./db')
 const sync = require('../db').sync
 const {
   User, Post, Reply,
-  Category, StoryLine } = require('../db').models
+  Location, Category, StoryLine } = require('../db').models
 
 const users = [
   { name: 'kaz', email: 'kaz@dita.net', username: 'kaz', password: 'dita', profilePic:'https://imagesvc.timeincapp.com/v3/fan/image?url=https://thesixersense.com/wp-content/uploads/getty-images/2017/08/830248940-2017-nba-rookie-photo-shoot.jpg.jpg&' },
   { name: 'wasif', email: 'wasif@dita.net', username: 'wasif', password: 'dita', profilePic:'http://a1.espncdn.com/combiner/i?img=%2Fphoto%2F2017%2F0303%2Fr186814_1296x729_16%2D9.jpg' },
   { name: 'vince', email: 'vince@riversconsulting.com', username: 'vince', password: 'dita', profilePic:'https://usatftw.files.wordpress.com/2017/11/usp_nba__philadelphia_76ers_at_los_angeles_lakers_95365191-e1511314321431.jpg?w=1000&h=600&crop=1'},
   { name: 'murray', email: 'murray@dita.net', 'username': 'murray', password: 'dita', profilePic:'https://img.bleacherreport.net/img/images/photos/003/706/425/d41845ec2e5fbd547a2be9b4be9d8e34_crop_north.jpg?h=533&w=800&q=70&crop_x=center&crop_y=top'}
+]
+
+const locations = [
+  { address: 'Saitama', lat: 36.0187669, lng: 139.0248439, isHome: true },
+  { address: 'New Hyde Park', lat: 40.7326609, lng: -73.6948277, isHome: true },
+  { address: 'Paterson', lat: 40.9151955, lng: -74.2028202, isHome: true },
+  { address: 'West Orange', lat: 40.7920341, lng: -74.2980747, isHome: true }
 ]
 
 const posts = [
@@ -78,6 +85,7 @@ const createCategories = () => {
 
 const createUsers = () => {
   return Promise.all(users.map(user => User.create(user)))
+    .then(_users => Promise.all(_users.map((user, i) => Location.addLocation(user.id, locations[i]))))
     .then(createStories)
 }
 

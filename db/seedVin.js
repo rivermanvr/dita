0515e0c76c67dfd80e45efb0257fa60f9331ad94
rivermanvr <vrgithub@riversconsulting.com>
2017-11-25 10:12:06 -0500

    modified seed file.

--- db/seedVin.js
+++ db/seedVin.js
@@ -6,8 +6,8 @@ const {
 
 const users = [
   { id: 4, name: 'vince', email: 'vince@rivcon.com', username: 'vince', password: 'dita', profilePic:'https://usatftw.files.wordpress.com/2017/11/usp_nba__philadelphia_76ers_at_los_angeles_lakers_95365191-e1511314321431.jpg?w=1000&h=600&crop=1'},
-  { id: 3, name: 'kaz', email: 'kaz@dita.net', username: 'kaz', password: 'dita', profilePic:'https://imagesvc.timeincapp.com/v3/fan/image?url=https://thesixersense.com/wp-content/uploads/getty-images/2017/08/830248940-2017-nba-rookie-photo-shoot.jpg.jpg&' },
-  { id: 2, name: 'wasif', email: 'wasif@dita.net', username: 'wasif', password: 'dita', profilePic:'http://a1.espncdn.com/combiner/i?img=%2Fphoto%2F2017%2F0303%2Fr186814_1296x729_16%2D9.jpg' },
+  { id: 2, name: 'kaz', email: 'kaz@dita.net', username: 'kaz', password: 'dita', profilePic:'https://imagesvc.timeincapp.com/v3/fan/image?url=https://thesixersense.com/wp-content/uploads/getty-images/2017/08/830248940-2017-nba-rookie-photo-shoot.jpg.jpg&' },
+  { id: 3, name: 'wasif', email: 'wasif@dita.net', username: 'wasif', password: 'dita', profilePic:'http://a1.espncdn.com/combiner/i?img=%2Fphoto%2F2017%2F0303%2Fr186814_1296x729_16%2D9.jpg' },
   { id: 1, name: 'murray', email: 'murray@dita.net', 'username': 'murray', password: 'dita', profilePic:'https://img.bleacherreport.net/img/images/photos/003/706/425/d41845ec2e5fbd547a2be9b4be9d8e34_crop_north.jpg?h=533&w=800&q=70&crop_x=center&crop_y=top'}
 ]
 

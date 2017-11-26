2dca6096e05e181b7f3d4c3ceece7c861752515c
Wasif Zaman <teatocode@gmail.com>
2017-11-26 09:11:58 -0500

    seperated environments

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -24,28 +24,9 @@ export default function () {
   return (
     <div className="container">
       {/* render on all paths */}
-      <Route component={ Navbar } />
 
       {/* render only on specific paths */}
       <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/login" component={ Login } />
-        <Route exact path="/signup" component={ Signup } />
-        <Route exact path="/profile" component={ Profile } />
-        <Route exact path="/testBed" component={ TestBed } />
-        <Route exact path="/google" component={ TestBedGoogle } />
-        <Route exact path="/testmover" component={ TestMover } />
-        <Route exact path="/storiesView" component={ StoriesView } />
-        <Route exact path="/postsView" component={ PostsView } />
-        <Route exact path="/posts" component={ PostForm } />
-        <Route exact path="/myposts" component={ Posts } />  
-        <Route exact path="/allposts" component={ AllPosts } />  
-        <Route exact path="/allpostsmap" component={ AllPostsMap } />
-        <Route exact path="/allpostsbyloc" component={ AllPostsByLoc } />  
-        <Route exact path="/distancetest" component={ TestDistance } />  
-        <Route path="/posts/:id" component={ PostDetail } />
-        <Route exact path="/createstory" component={ CreateStory } />  
-        <Route exact path="/mylocations" component={ MyLocations } />  
       </Switch>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Dev from './components/Dev';
 import App from './components/App';
 
 ReactDOM.render(
   <Provider store={ store }>
     <Router>
-      <App />
+      <Switch>
+        <Route path='/dev' component={ Dev } />
+        <Route path='/' component={ App } />
+      </Switch>
     </Router>
   </Provider>,
   document.getElementById('root'));

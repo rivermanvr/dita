25289b4ded53d2efb6fb3233d2ef1f215aa05974
Wasif Zaman <teatocode@gmail.com>
2017-11-26 20:07:01 -0500

    comment for top nav

--- src/components/App.js
+++ src/components/App.js
@@ -24,7 +24,7 @@ export default class App extends Component {
         {/* styles */}
         <link href="/public/css/style.css" rel="stylesheet" />
 
-        {/* */}
+        {/* top nav */}
         <Nav />
 
         {/* temp headline */}

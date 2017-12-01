7e8cd24990ac407a895342822e35bfdcb20c05f4
Wasif Zaman <teatocode@gmail.com>
2017-12-01 17:09:38 -0500

    ready for review

--- src/components/pages/dashboardComponents/DashboardMyPosts.js
+++ src/components/pages/dashboardComponents/DashboardMyPosts.js
@@ -4,7 +4,7 @@ import * as d3 from 'd3'
 import { Link } from 'react-router-dom'
 import Replies from '../../singleFunction/Replies'
 
-export const PostCard = ({ content }) => {
+export const xPostCard = ({ content }) => {
   if (!content) return <div></div>
 
   const {
@@ -39,10 +39,29 @@ const Posts = ({ userPosts, pathname }) => {
 
   return (
     <div className='dashboard-item'>
-      <div className='dashboard-header'>
-          <h3>My Posts</h3>
+      <h3 className='dashboard-header'>My Posts</h3>
+
+      <div className='post-cards-container'>
+      { userPosts && userPosts.map(post => {
+        let title = (post.title || post.body).slice(0, 30)
+        return <div key={ post.id } className='post-card'>
+          <div className='post-card-header'>
+            <h4>{ `${title}${title.length > 30 ? '...' : ''}` }</h4>
+            <span className={ `trending-status hl-${Math.ceil(post.halflife)}` }></span>
+          </div>
+
+          <div className='post-card-body'>
+            <p>{ post.body }</p>
+          </div>
+
+          <div className='post-card-footer'>
+            <span>{ d3.timeFormat('%m/%d/%y')(new Date(post.createdAt)) }</span>
+            <span className='replies-count-container'><i className='ion-ios-chatboxes-outline'></i> <span className='replies-count'>{ post.replies.length }</span></span>
+            <span onClick={ () => console.log('open modal') } className='more'><i className='ion-ios-more-outline'></i></span>
+          </div>
         </div>
-      { posts.map(post => <PostCard key={ post.id } content={ post } />) }
+      })}
+      </div>
     </div>
   )
 }
